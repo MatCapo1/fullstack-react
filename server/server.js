19852f51a5e2fa4f83ee5cacea58baa2b8d72bc0
Matias <matias@MBP-de-oscar.fibertel.com.ar>
2021-09-26 20:34:39 -0300

    Modified server/server.js file

--- server/server.js
+++ server/server.js
@@ -3,5 +3,22 @@ import express from 'express'
 const app = express()
 
 import devBundle from './devBundle'
-const app = express ()
+const app = express()
 devBundle.compile(app)
+
+import path from 'path'
+const CURREN_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURREN_WORKING_DIR,'dist')))
+
+import template from './../template'
+app.get ('/', (req, res) => {
+    res.status(200).send(template ())
+})
+
+let port = procces.env.PORT || 3000
+app.listen(port, function onStart(err) {
+    if (err){
+        console.log(err)
+    }
+    console.info('Server started on port %s.', port)
+})
